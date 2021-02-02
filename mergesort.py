270f662875da8f94cf0c6e7e11b5f6b1e9f5182d
quechulo <78055811+quechulo@users.noreply.github.com>
2021-02-02 15:36:26 +0100

    Add files via upload

---
+++ mergesort.py
@@ -0,0 +1,36 @@
+"""
+    Implementation of merge sort algorithm
+"""
+__author__ = "Michał Podolec"
+
+def merge_sort(lo, hi, tab, a): # function to divide array and initialise recursive execution
+    if hi > lo:
+        mid = lo + (hi - lo) // 2
+        merge_sort(lo, mid, tab, a)
+        merge_sort(mid+1, hi, tab, a)
+        merge_merge(lo, hi, mid, tab, a)
+    return tab
+
+def merge_merge(lo, hi, mid, tab, a):
+    for k in range(lo, hi+1):
+        a[k] = tab[k]
+    i = lo
+    j = mid+1
+
+    for k in range(lo, hi+1):
+        if i > mid:
+            tab[k] = a[j]
+            j += 1
+        elif j > hi:
+            tab[k] = a[i]
+            i += 1
+        elif a[i] > a[j]:
+            tab[k] = a[j]
+            j += 1
+        else:
+            tab[k] = a[i]
+            i += 1
+
+def run_merge_sort(tab):
+    return merge_sort(0, len(tab)-1, tab, tab.copy())
+
